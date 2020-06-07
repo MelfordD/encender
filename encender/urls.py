f5b2a2d0bdf826144f431274a672f3d8df84efaa
Melford Dike <melford.deekay@gmail.com>
2020-06-07 19:16:45 +0100

    fixing static 2

--- encender/urls.py
+++ encender/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
